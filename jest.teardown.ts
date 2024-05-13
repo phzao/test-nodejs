9129590c57f7d3116e1d37e7b089b2db0a81fb5b
PH <ph@ph>
2024-05-13 08:46:00 -0300

    finishing the step 5

--- jest.teardown.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-export default async () => {
-  await mongoose.connection.dropDatabase();
-  await mongoose.connection.close();
-};
-
---
+++ src/infrastructure/repositories/comments.repository.ts
@@ -0,0 +1,38 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Comment, CommentDocument } from '@domain/comments/comment.entity';
+
+@Injectable()
+export class CommentRepository {
+  constructor(
+    @InjectModel(Comment.name)
+    private readonly commentModel: Model<CommentDocument>,
+  ) {}
+
+  async findByPostId(postId: string): Promise<Comment[]> {
+    return this.commentModel.find({ post_id: postId }).exec();
+  }
+
+  async findById(id: string): Promise<Comment | null> {
+    return this.commentModel.findById(id).exec();
+  }
+
+  async create(comment: Comment): Promise<Comment> {
+    const newComment = new this.commentModel(comment);
+    return newComment.save();
+  }
+
+  async update(
+    id: string,
+    updatedComment: Partial<Comment>,
+  ): Promise<Comment | null> {
+    return this.commentModel
+      .findByIdAndUpdate(id, updatedComment, { new: true })
+      .exec();
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.commentModel.findByIdAndDelete(id).exec();
+  }
+}
